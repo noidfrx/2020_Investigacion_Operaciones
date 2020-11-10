d84be2aed10eefd1c9cc1efa4d18c7fe5b8d429a
noidfrx <58480414+noidfrx@users.noreply.github.com>
2020-11-10 13:11:29 -0300

    Tercer

--- Investigacion_Operaciones_2020/src/start/Main.java
+++ Investigacion_Operaciones_2020/src/start/Main.java
@@ -10,6 +10,7 @@ public class Main {
 public static void main(String[] args)throws IOException {
 		System.out.print("Hola mundo!");
 		SRFLP claseProfe = new SRFLP("C:\\Users\\edufc\\git\\2020_Investigacion_Operaciones\\Investigacion_Operaciones_2020\\src\\S10.txt");
+		//SOy un comentario
 	}	
 
 }
