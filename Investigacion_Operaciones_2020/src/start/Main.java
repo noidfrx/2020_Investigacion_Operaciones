2cc7e4c523b83a8b3c64ceef334651913a3d239f
noidfrx <58480414+noidfrx@users.noreply.github.com>
2020-11-10 12:54:40 -0300

    Initial commit

---
+++ Investigacion_Operaciones_2020/src/start/Main.java
@@ -0,0 +1,15 @@
+package start;
+
+import java.io.IOException;
+
+import lectura.SRFLP;
+
+
+
+public class Main {
+public static void main(String[] args)throws IOException {
+		System.out.print("Hola mundo!");
+		SRFLP claseProfe = new SRFLP("C:\\Users\\edufc\\eclipse-workspace\\Investigacion_Operaciones_2020\\src\\S10.txt");
+	}	
+
+}
