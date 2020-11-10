e4155a6a8555633fa8e587e5950a8e58fb03792b
noidfrx <58480414+noidfrx@users.noreply.github.com>
2020-11-10 13:11:11 -0300

    Second commit

--- Investigacion_Operaciones_2020/src/start/Main.java
+++ Investigacion_Operaciones_2020/src/start/Main.java
@@ -9,7 +9,7 @@ import lectura.SRFLP;
 public class Main {
 public static void main(String[] args)throws IOException {
 		System.out.print("Hola mundo!");
-		SRFLP claseProfe = new SRFLP("C:\\Users\\edufc\\eclipse-workspace\\Investigacion_Operaciones_2020\\src\\S10.txt");
+		SRFLP claseProfe = new SRFLP("C:\\Users\\edufc\\git\\2020_Investigacion_Operaciones\\Investigacion_Operaciones_2020\\src\\S10.txt");
 	}	
 
 }
